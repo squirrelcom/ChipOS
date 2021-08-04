caa9c6cf6f2b4c855aa01658384068044c30e69e
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-04 23:22:10 +0100

    Update status-arroyo.js

--- html/screens/status-arroyo.js
+++ html/screens/status-arroyo.js
@@ -1,11 +1,9 @@
 screens['status-arroyo'] = '\
 <br/>\
 <ul class="status">\
-  <li style="text-decoration: underline">Arroyo Status</li>\
-  <li class="done">1. Rescue Nagor\'s dog, Smoke, from the wilds.</li>\
-  <li>2. Find Vic the Trader</li>\
-  <li class="done">3. Obtain flint to have Mynoc sharpen your spear.</li>\
-  <li class="done">4. Kill the evil plants that infest Hakunin\'s garden.</li>\
-  <li>5. Retrieve the GECK for Arroyo.</li>\
+  <li style="text-decoration: underline">ABOUT</li>\
+  <li>ChipOS v0.1 beta</li>\
+  <li>COPYRIGHT 2021 Squirrel Computers (R)</li>\
+  <li>LOADER v1.0</li>\
 </ul>\
 <br/><br/><br/><br/><br/><br/><br/><br/>' + statusBack;
