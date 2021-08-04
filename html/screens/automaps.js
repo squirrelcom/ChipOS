9e8c27cb9eb722115ed265b31a07dcb48edf4e67
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-04 23:20:24 +0100

    Update automaps.js

--- html/screens/automaps.js
+++ html/screens/automaps.js
@@ -1,4 +1,4 @@
-screens.automaps = '<h2 class="title">AUTOMAPS</h2>\
+screens.automaps = '<h2 class="title">CHIPMAPS</h2>\
 <ul class="status">\
   <li class="c" onclick="page(\'automaps-arroyo-temple\', true)">Deal</li>\
   <li class="c"onclick="page(\'automaps-den-businesses-1\', true)">Dover</li>\
