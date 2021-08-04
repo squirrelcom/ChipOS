8ee1e3d33e41387e2709635a4da1009603a93a54
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-04 23:16:43 +0100

    Update automaps-klamath-mall.js

--- html/screens/automaps-klamath-mall.js
+++ html/screens/automaps-klamath-mall.js
@@ -1,6 +1,2 @@
 screens['automaps-klamath-mall'] =
-  '<h2 class="title">AUTOMAPS</h2>'
-  + '<h3>Klamath</h3>'
-  + automapsKlamathNavi
-  + '<img class="map" src="img/automaps-klamath-mall.png"/>'
-  + automapsBack;
+  + '<img class="map" src="img/automaps-klamath-mall.gif"/>'
