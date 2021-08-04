d2bd753e0117422a4a921ad4b30325145ddfa443
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-04 23:13:39 +0100

    Update archives-suit.js

--- html/screens/archives-suit.js
+++ html/screens/archives-suit.js
@@ -1,4 +1,4 @@
 screens['archives-suit'] = '\
 <br/>\
-<iframe width="480" src="https://www.youtube.com/embed/vuo2I_CJOhQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope" allowfullscreen></iframe>\
+<iframe src="https://free.timeanddate.com/clock/i7x93j1l/n3843/fs22/fc090/tc000/pc000/ftbi/tt0/tw1/tm1/ts1/tb4" frameborder="0" width="196" height="53"></iframe>\
 <br/><br/><br/><br/>' + archivesBack;
