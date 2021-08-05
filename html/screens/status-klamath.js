676440a593be95f50ca102e653d613482a162f4c
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 09:00:38 +0100

    Update status-klamath.js

--- html/screens/status-klamath.js
+++ html/screens/status-klamath.js
@@ -1,10 +1,4 @@
 screens['status-klamath'] = '\
 <br/>\
-<ul class="status">\
-  <li style="text-decoration: underline">Klamath Status</li>\
-  <li class="done">1. Refuel the still.</li>\
-  <li class="done">2. Rescue Smiley the Trapper.</li>\
-  <li class="done">3. Guard the brahmin.</li>\
-  <li class="done">4. Kill the rat god.</li>\
-</ul>\
+    '<img class="map" src="img/automaps-klamath-mall.png/>'
 <br/><br/><br/><br/><br/><br/><br/><br/>' + statusBack;
