7515fe836aae15151f1e9d4248c7754adb743fdd
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 09:00:55 +0100

    Update status-klamath.js

--- html/screens/status-klamath.js
+++ html/screens/status-klamath.js
@@ -1,4 +1,4 @@
 screens['status-klamath'] = '\
 <br/>\
-    '<img class="map" src="img/automaps-klamath-mall.png/>'
+    '<img class="map" src="img/automaps-klamath-mall.png/>'\
 <br/><br/><br/><br/><br/><br/><br/><br/>' + statusBack;
