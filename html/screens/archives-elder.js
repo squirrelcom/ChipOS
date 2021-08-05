e978ab1678d32a8df4bbbaf69684429ebe873241
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 12:21:13 +0100

    Update archives-elder.js

--- html/screens/archives-elder.js
+++ html/screens/archives-elder.js
@@ -1,5 +1,5 @@
 screens['archives-elder'] = '\
 <br/>\
 <br/>\
-<iframe src="11.html" frameborder="0" width="196" height="53"></iframe>\
+<iframe src="11.html" frameborder="0" width="260" height="160"></iframe>\
 <br/><br/><br/><br/>' + archivesBack;
