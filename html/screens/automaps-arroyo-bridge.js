be8d5a78ec595ec4ce9bd87e41cb6da3824ae41b
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 08:42:26 +0100

    Update automaps-arroyo-bridge.js

--- html/screens/automaps-arroyo-bridge.js
+++ html/screens/automaps-arroyo-bridge.js
@@ -1,6 +1,5 @@
 screens['automaps-arroyo-bridge'] =
-  '<h2 class="title">AUTOMAPS</h2>'
-  + '<h3>Arroyo</h3>'
-  + automapsArroyoNavi
+  '<h2 class="title">CHIPMAPS</h2>'
+  + '<h3>Deal</h3>'
   + '<img class="map" src="img/automaps-arroyo-bridge.png"/>'
   + automapsBack;
