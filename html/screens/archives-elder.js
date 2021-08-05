73c8b4d60ba4c7996c2313d0d94aef82f757a239
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 12:18:00 +0100

    Update archives-elder.js

--- html/screens/archives-elder.js
+++ html/screens/archives-elder.js
@@ -1,6 +1,5 @@
-const archivesBack = '<ul class="back"><li class="c" onclick="page(\'archives\', true)">BACK</li></ul>';
-
 screens['archives-elder'] = '\
 <br/>\
-<iframe width="480" src="https://www.youtube.com/embed/JtS1KDVg27c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope" allowfullscreen></iframe>\
+<br/>\
+<iframe src="11.html" frameborder="0" width="196" height="53"></iframe>\
 <br/><br/><br/><br/>' + archivesBack;
