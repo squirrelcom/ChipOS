9f3f5775926dcff45b64adaf612cc8194bb9369c
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 13:02:17 +0100

    Update archives.js

--- html/screens/archives.js
+++ html/screens/archives.js
@@ -1,6 +1,6 @@
-screens.archives = '<h2 class="title">CHIPCLOCK</h2>\
+screens.archives = '<h2 class="title">LIBARY</h2>\
 <ul class="status">\
-  <li class="c" onclick="page(\'archives-suit\', true)">NEW CLOCK</li>\
+  <li class="c" onclick="page(\'archives-suit\', true)">CHIPCLOCK</li>\
   <li class="c" onclick="page(\'archives-dream-2\', true)">SNAKE</li>\
 </ul>\
 ';
