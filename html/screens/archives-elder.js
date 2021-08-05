e7bc6595c5160da259bcf60683d53f4cac7f591e
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 12:33:29 +0100

    Update archives-elder.js

--- html/screens/archives-elder.js
+++ html/screens/archives-elder.js
@@ -1,3 +1,5 @@
+const archivesBack = '<ul class="back"><li class="c" onclick="page(\'archives\', true)">BACK</li></ul>';
+
 screens['archives-elder'] = '\
 <br/>\
 <iframe src="https://squirrelcom.github.io/snake/" frameborder="0" width="260" height="160"></iframe>\
