bae71b563b451af1262709029fd7d194b0ca4e80
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-05 12:37:19 +0100

    Update archives-dream-2.js

--- html/screens/archives-dream-2.js
+++ html/screens/archives-dream-2.js
@@ -1,4 +1,4 @@
 screens['archives-dream-2'] = '\
 <br/>\
-<iframe src="11.html" frameborder="0" width="260" height="160"></iframe>\
+<iframe src="11.html" frameborder="0" width="480" height="480"></iframe>\
 <br/><br/><br/><br/>' + archivesBack;
