2044c4831d5413b4abcb923e909a9fb1f3be9cdf
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-06 08:20:30 +0100

    Update automaps-arroyo-village.js

--- html/screens/automaps-arroyo-village.js
+++ html/screens/automaps-arroyo-village.js
@@ -1,6 +1,6 @@
 screens['automaps-arroyo-village'] =
-  '<h2 class="title">AUTOMAPS</h2>'
-  + '<h3>Arroyo</h3>'
+  '<h2 class="title">CHIPMAPS</h2>'
+  + '<h3>DEAL</h3>'
   + automapsArroyoNavi
   + '<img class="map" src="img/automaps-arroyo-village.png"/>'
   + automapsBack;
