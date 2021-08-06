a5b19c3981cfd8c040f5963be8daed7f4192b5ec
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-06 07:23:32 +0100

    Update archives.js

--- html/screens/archives.js
+++ html/screens/archives.js
@@ -3,5 +3,6 @@ screens.archives = '<h2 class="title">LIBARY</h2>\
   <li class="c" onclick="page(\'archives-suit\', true)">CHIPCLOCK</li>\
   <li class="c" onclick="page(\'archives-dream-2\', true)">SNAKE</li>\
   <li class="c" onclick="page(\'automaps\', true)">CHIPMAPS</li>\
+  <li class="c" onclick="page(\'archives-dream-1\', true)">TEMPERATURE</li>\
 </ul>\
 ';
