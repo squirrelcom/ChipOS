97c26ba78d3a48daee5c06905ed7516ac4f9ebba
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-06 08:18:21 +0100

    Update automaps-arroyo-temple.js

--- html/screens/automaps-arroyo-temple.js
+++ html/screens/automaps-arroyo-temple.js
@@ -1,15 +1,12 @@
 const automapsArroyoNavi = '<ul class="status">\
-  <li class="c" onclick="page(\'automaps-arroyo-temple\')">Temple: Inner Sanctum</li>\
-  <li class="c" onclick="page(\'automaps-arroyo-village\')">Village</li>\
-  <li class="c" onclick="page(\'automaps-arroyo-bridge\')">Bridge</li>\
-  <li class="c" onclick="page(\'automaps-arroyo-wilderness\')">Wilderness</li>\
-  <li class="c" onclick="page(\'automaps-arroyo-temple-entrance\')">Temple Entrance</li>\
+  <li class="c" onclick="page(\'automaps-arroyo-temple\')">Part 1</li>\
+  <li class="c" onclick="page(\'automaps-arroyo-village\')">Part 2</li>\
 </ul>\
 ';
 
 screens['automaps-arroyo-temple'] =
-  '<h2 class="title">AUTOMAPS</h2>'
-  + '<h3>Arroyo</h3>'
+  '<h2 class="title">CHIPMAPS</h2>'
+  + '<h3>DEAL</h3>'
   + automapsArroyoNavi
   + '<img class="map" src="img/automaps-arroyo-temple.png"/>'
   + automapsBack;
