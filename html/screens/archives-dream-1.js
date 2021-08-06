9c3cebc5e083eab45097debc5a14bf611ba253f3
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-06 07:54:14 +0100

    Update archives-dream-1.js

--- html/screens/archives-dream-1.js
+++ html/screens/archives-dream-1.js
@@ -1,4 +1,5 @@
 screens['archives-dream-1'] = '\
 <br/>\
+<h2 class="title">TEMPERATURE</h2>\
 <iframe src="temp.html" frameborder="0" width="420" height="420"></iframe>\
 <br/><br/><br/><br/>' + archivesBack;
