e4f943da11a78fd5ad9cd02ccd703b65c8452298
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-17 08:32:34 +0100

    Update archives-suit.js
    42

--- html/screens/archives-suit.js
+++ html/screens/archives-suit.js
@@ -1,5 +1,5 @@
 screens['archives-suit'] = '\
 <br/>\
 <h2 class="title">CHIPCLOCK</h2>\
-<iframe src="33.html" frameborder="0" width="196" height="53"></iframe>\
+<iframe src="33.html" frameborder="0" width="420" height="420"></iframe>\
 <br/><br/><br/><br/>' + archivesBack;
